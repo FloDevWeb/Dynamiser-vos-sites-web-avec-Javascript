059dd1368bcfd5f4ec10551eae068501578a3cf1
Flo <flodevweb@oulook.fr>
2023-02-22 21:58:00 +0100

    function newGame & roll

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const displayGlobal1 = document.getElementById("global_1");
 const displayGlobal2 = document.getElementById("global_2");
 const displayRound1 = document.getElementById("round_1");
 const displayRound2 = document.getElementById("round_2");
-const roll = document.getElementById("roll");
+const rollDice = document.getElementById("roll");
 const hold = document.getElementById("hold");
 
 //fonction pour obtenir un résultat aléatoire
@@ -19,3 +19,43 @@ let currentScore2 = 0;
 let totalScore2 = 0;
 let currentPlayer = 1;
 let value = 0;
+
+function newGame() {
+  currentScore1 = 0;
+  totalScore1 = 0;
+  currentScore2 = 0;
+  totalScore2 = 0;
+  currentPlayer = 1;
+  displayGlobal1.innerHTML = 0;
+  displayGlobal2.innerHTML = 0;
+  displayRound1.innerHTML = 0;
+  displayRound2.innerHTML = 0;
+}
+
+newGameBtn.addEventListener("click", newGame);
+
+function roll() {
+  value = getRandomInt(1, 7);
+  console.log(value);
+  if (value > 1) {
+    if (currentPlayer === 1) {
+      currentScore1 += value;
+      displayRound1.innerHTML = currentScore1;
+    } else {
+      currentScore2 += value;
+      displayRound2.innerHTML = currentScore2;
+    }
+  } else {
+    if (currentPlayer === 1) {
+      currentScore1 = 0;
+      displayRound1.innerHTML = currentScore1;
+      currentPlayer = 2;
+    } else {
+      currentScore2 = 0;
+      displayRound2.innerHTML = currentScore2;
+      currentPlayer = 1;
+    }
+  }
+}
+
+rollDice.addEventListener("click", roll);
